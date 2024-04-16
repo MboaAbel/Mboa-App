0d9bac40b75b099c70d9e730d55840b8fe6ab6ee
mboaabel <mboaacademy@gmail.com>
2024-04-16 15:17:05 +0300

    first commit

--- duka/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import MboaNFTs, index, biz
-
-urlpatterns = [
-	path('index/', index, name='index'),
-	path('biz', biz, name='biz'),
-	path('MboaNFTs', MboaNFTs, name='MboaNFTs'),
-
-]
